556f686208107ac8635e08199dd69edd6ac99742
Kenny <kenny.olofsson23@gmail.com>
2026-03-19 22:20:30 +0100

    pipeline: complete build-features

--- src/__tests__/test-store-minimal.test.ts
+++ src/__tests__/test-store-minimal.test.ts
@@ -1,5 +1,9 @@
 import { useResumeStore } from '@/store/resume-store'
 
+beforeEach(() => {
+  useResumeStore.setState({ resume: null, pastStates: [], futureStates: [] })
+})
+
 describe('minimal store test', () => {
   it('store exists', () => {
     expect(useResumeStore).toBeDefined()
